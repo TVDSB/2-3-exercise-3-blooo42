3a671d0dbe47f2de0349613c15287baa224d900b
blooo42 <100421892+blooo42@users.noreply.github.com>
2022-03-31 08:50:12 -0400

    Update student.py

--- student.py
+++ student.py
@@ -1,20 +1,15 @@
 def main():
     #your code goes here
-    percentage = int(input("enter your mark percent here \n"))
+    number = int(input("enter a number here \n"))
 
-    if percentage >= 80 and percentage <=100:
-       print("you got an A!")
+    if number%15 == 0:
+        print("fizzbuzz")
 
-    elif percentage >= 70:
-       print("you got a B")
+    elif number%3 == 0:
+         print("fizz")
 
-    elif percentage >= 60:
-       print("you got a C")
+    elif number%5 == 0:
+         print("buzz")
 
-    elif percentage >= 50:
-       print("you got a D")
-
-    elif percentage >=0 and percentage <=49:
-       print("you got an F")
 if __name__ =='__main__':
     main()
